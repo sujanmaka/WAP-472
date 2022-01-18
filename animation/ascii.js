20c916d4071e5105c80a234b876bdbf7ab0eb551
sujan maka <sujan.maka@gmail.com>
2022-01-17 20:59:31 -0600

    fixes

--- animation/ascii.js
+++ animation/ascii.js
@@ -5,16 +5,18 @@ import { ANIMATIONS } from './animation.js';
 var myinterval;
 //by default, stop button is disabled at beginning
 document.getElementById("stop").disabled = true;
+document.getElementById("start").disabled = true;
+
+let speed = 250;
 
 //function to perform start action
-export function start() {
+export function start(val) {
     document.getElementById("start").disabled = true;
     document.getElementById("stop").disabled = false;
 
     let count = 0;
-    let speed = 250;
-    if (document.getElementById("speed").checked) {
-        speed = 50;
+    if (val) {
+        speed = val;
     }
 
 
@@ -22,18 +24,18 @@ export function start() {
     let item = selectedItem.options[selectedItem.selectedIndex].value;
 
     //split the content into contents array
-    const contents = ANIMATIONS[item].split("=====\n");
+    let contents;
+    if (item == "CUSTOM") {
+        contents = document.getElementById("screen").value.split("=====\n");
+    } else if (item == "BLANK") {
+        contents = [];
+    } else {
+        contents = ANIMATIONS[item].split("=====\n");
+    }
 
     let selectedSize = document.getElementById('itemSize');
     let size = selectedSize.options[selectedSize.selectedIndex].value;
     document.getElementById("screen").style.fontSize = size;
-
-    //animation logic for CUSTOM
-    // if (item == "CUSTOM") {
-    //     let content = document.getElementById("screen").value;
-    //     contents = [content.toUpperCase(), content.toLowerCase(), content.toUpperCase()];
-    // }
-    // console.log(contents);
 
     //calls interval function on particular time 
     myinterval = setInterval(function() {
@@ -59,5 +61,30 @@ export function stop() {
 
 //repetitive action to display the content of array as animation
 function displayHello(myArray, count) {
-    document.getElementById("screen").innerHTML = myArray[count];
+    if (myArray.length > 0) {
+        document.getElementById("screen").innerHTML = myArray[count];
+    }
+}
+
+
+export function checkboxAction() {
+    clearInterval(myinterval);
+    if (document.getElementById("speed").checked) {
+        speed = 50;
+    } else {
+        speed = 250;
+    }
+    start(speed);
+}
+
+
+export function itemSelectAction() {
+    let selectedItem = document.getElementById('item');
+    let item = selectedItem.options[selectedItem.selectedIndex].value;
+    if (item == "BLANK") {
+        document.getElementById("stop").disabled = true;
+        document.getElementById("start").disabled = true;
+    } else {
+        document.getElementById("start").disabled = false;
+    }
 }
