f671ad9c5cc10a0650d4c18cae184346ec2e970f
sujan maka <sujan.maka@gmail.com>
2022-01-18 14:51:09 -0600

    module pattern lab

---
+++ W2D3/lab/solution2/bankingAccount.js
@@ -0,0 +1,30 @@
+window.onload = function() {
+
+    let accountInfoList = [];
+    let button = document.getElementById("createBtn");
+
+    class Account {
+        constructor(name, deposit) {
+            this.name = name;
+            this.deposit = deposit;
+        }
+    }
+    let createAccount = (function() {
+        let name = document.getElementById("name");
+        let deposit = document.getElementById("deposit");
+        let textarea = document.getElementById("textarea");
+        return function() {
+
+            accountInfoList.push(new Account(name.value, deposit.value));
+            let output = "";
+            for (let i = 0; i < accountInfoList.length; i++) {
+                output += "Account name: " + accountInfoList[i].name +
+                    "   Balance: " + accountInfoList[i].deposit + "\n";
+            }
+            textarea.value = output;
+        };
+    })();
+
+    button.onclick = createAccount;
+
+};
