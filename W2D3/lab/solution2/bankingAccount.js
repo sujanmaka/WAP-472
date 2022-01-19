5b6aeae92b8ccba03f9ae9563bf391768fcb9ce3
sujan maka <sujan.maka@gmail.com>
2022-01-19 08:20:57 -0600

    optimization

--- W2D3/lab/solution2/bankingAccount.js
+++ W2D3/lab/solution2/bankingAccount.js
@@ -13,15 +13,17 @@ window.onload = function() {
         let name = document.getElementById("name");
         let deposit = document.getElementById("deposit");
         let textarea = document.getElementById("textarea");
-        return function() {
-
-            accountInfoList.push(new Account(name.value, deposit.value));
+        let display = function() {
             let output = "";
             for (let i = 0; i < accountInfoList.length; i++) {
                 output += "Account name: " + accountInfoList[i].name +
                     "   Balance: " + accountInfoList[i].deposit + "\n";
             }
             textarea.value = output;
+        }
+        return function() {
+            accountInfoList.push(new Account(name.value, deposit.value));
+            display();
         };
     })();
 
