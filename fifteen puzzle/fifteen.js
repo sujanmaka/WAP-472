f1f6fe90f8096a6f1b9920df7b77b6183f648d4b
sujan maka <sujan.maka@gmail.com>
2022-01-24 21:54:04 -0600

    feat: puzzle

--- fifteen puzzle/fifteen.js
+++ fifteen puzzle/fifteen.js
@@ -20,75 +20,70 @@ $(function() {
 
     //code to shuffle the tiles
     $("#controls").click(function() {
-        console.log("vacant " + vacantX + " " + vacantY);
         const values = [0, 100, 200, 300];
 
-        for (let i = 0; i < 100; i++) {
+        for (let i = 0; i < 200; i++) {
             const randomX = Math.floor(Math.random() * values.length);
             const randomY = Math.floor(Math.random() * values.length);
 
             let tempX = values[randomX];
             let tempY = values[randomY];
-            console.log("random " + tempX + " " + tempY);
-
-
-            // if (tempX == vacantX && tempY == vacantY) {
-            //     console.log("blank detected");
-            //     continue;
-            // }
-            // let tempX = 0;
-            // let tempY = 0;
             let tile = "#square_" + tempX + "_" + tempY;
-            // $(tile).attr("id", "square_" + vacantX + "_" + vacantY);
-
-
-            $(tile).attr("x", vacantX);
-            $(tile).attr("y", vacantY);
-            let x = vacantX;
-            let y = vacantY;
-
-            $(tile).css({
-                "left": x + 'px',
-                "top": y + 'px',
-                "backgroundImage": 'url("background.jpg")',
-                "backgroundPosition": -x + 'px ' + (-y) + 'px'
-            });
-
-            $(tile).attr("id", "square_" + vacantX + "_" + vacantY);
-            vacantX = tempX;
-            vacantY = tempY;
+            ({ vacantX, vacantY } = moveTile(tile, vacantX, vacantY, tempX, tempY));
         }
 
 
     });
 
+    //hover on movable title
+    $(".puzzlepiece").hover(function() {
+        let tempX = parseInt($(this).attr("x"));
+        let tempY = parseInt($(this).attr("y"));
+        if (isMovable(tempX, vacantX, tempY, vacantY)) {
+            $(this).css({ "border-color": "red", "color": "red" });
+        }
+    }, function() {
+        $(this).css({ "border-color": "black", "color": "black" });
+    });
+
+    //click on movable title
     $(".puzzlepiece").click(function() {
         let tempX = parseInt($(this).attr("x"));
         let tempY = parseInt($(this).attr("y"));
-        if ((tempX + 100 == vacantX && tempY == vacantY) ||
-            (tempX - 100 == vacantX && tempY == vacantY) ||
-            (tempY + 100 == vacantY && tempX == vacantX) ||
-            (tempY - 100 == vacantY && tempX == vacantX)) {
-            $(this).attr("x", vacantX);
-            $(this).attr("y", vacantY);
-            let x = vacantX;
-            let y = vacantY;
+        if (isMovable(tempX, vacantX, tempY, vacantY)) {
+            ({ vacantX, vacantY } = moveTile(this, vacantX, vacantY, tempX, tempY));
 
-            $(this).css({
-                "left": x + 'px',
-                "top": y + 'px',
-                "backgroundImage": 'url("background.jpg")',
-                "backgroundPosition": -x + 'px ' + (-y) + 'px'
-            });
-            $(this).attr("id", "square_" + vacantX + "_" + vacantY);
-            vacantX = tempX;
-            vacantY = tempY;
         } else {
             console.log(" can't move");
-
         }
 
     });
 
 
 });
+
+function isMovable(tempX, vacantX, tempY, vacantY) {
+    return (tempX + 100 == vacantX && tempY == vacantY) ||
+        (tempX - 100 == vacantX && tempY == vacantY) ||
+        (tempY + 100 == vacantY && tempX == vacantX) ||
+        (tempY - 100 == vacantY && tempX == vacantX);
+}
+
+function moveTile(tile, vacantX, vacantY, tempX, tempY) {
+    $(tile).attr("x", vacantX);
+    $(tile).attr("y", vacantY);
+    let x = vacantX;
+    let y = vacantY;
+
+    $(tile).css({
+        "left": x + 'px',
+        "top": y + 'px',
+        "backgroundImage": 'url("background.jpg")',
+        "backgroundPosition": -x + 'px ' + (-y) + 'px'
+    });
+
+    $(tile).attr("id", "square_" + vacantX + "_" + vacantY);
+    vacantX = tempX;
+    vacantY = tempY;
+    return { vacantX, vacantY };
+}
